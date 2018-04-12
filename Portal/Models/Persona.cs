c2ab8f7faaf48d240478cd6bfda48b86d2f2872f
Diego Urrutia-Astorga <durrutia@ucn.cl>
2018-04-11 23:27:26 -0300

    Added: Anotaciones de control de atributos.

--- Portal/Models/Persona.cs
+++ Portal/Models/Persona.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +9,21 @@ namespace Portal.Models
 {
     public class Persona : BaseEntity
     {
+        [DisplayName("R.U.T.")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:N}")]
         public string RUT { get; set; }
 
+        [Required(ErrorMessage = "Se requiere el nombre")]
+        [StringLength(40)]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "Se requiere el apellido paterno")]
+        [StringLength(80)]
         public string Paterno { get; set; }
 
         public string Materno { get; set; }
 
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public override string ToString()
